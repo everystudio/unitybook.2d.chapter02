9005bca50ce9cac5747b35db69b029030689ebd6
watanabe@marida <watanabe.everystudio@gmail.com>
2022-12-31 14:38:15 +0900

    テキストを更新する処理について

--- Assets/Scripts/RouletteController.cs
+++ Assets/Scripts/RouletteController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RouletteController : MonoBehaviour
 {
     private Rigidbody2D myRigidboyd;
     [SerializeField] private float currentAngle;
+    [SerializeField] private TextMeshProUGUI weatherText;
+
     private void Start()
     {
         myRigidboyd = GetComponent<Rigidbody2D>();
         myRigidboyd.AddTorque(5f, ForceMode2D.Impulse);
+        weatherText.text = "文字を変更";
     }
     void Update()
     {
