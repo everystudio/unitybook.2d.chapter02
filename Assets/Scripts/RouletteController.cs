895a995f04ae0789f30ca187f4b9b4f0ec7a77fd
watanabe@marida <watanabe.everystudio@gmail.com>
2022-12-31 15:21:54 +0900

    ルーレットの開始終了

--- Assets/Scripts/RouletteController.cs
+++ Assets/Scripts/RouletteController.cs
@@ -12,11 +12,40 @@ public class RouletteController : MonoBehaviour
     private void Start()
     {
         myRigidboyd = GetComponent<Rigidbody2D>();
-        myRigidboyd.AddTorque(5f, ForceMode2D.Impulse);
-        weatherText.text = "文字を変更";
+        weatherText.text = "change text";
     }
     void Update()
     {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            myRigidboyd.AddTorque(5f, ForceMode2D.Impulse);
+            myRigidboyd.angularDrag = 0f;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            myRigidboyd.angularDrag = 0.75f;
+        }
+
         currentAngle = Vector3.SignedAngle(transform.up, Vector3.up, new Vector3(0f, 0f, -1f));
+        currentAngle += 180f;
+
+        if (currentAngle < 90f)
+        {
+            weatherText.text = "Moon";
+        }
+        else if (currentAngle < 180f)
+        {
+            weatherText.text = "Rain";
+        }
+        else if (currentAngle < 270f)
+        {
+            weatherText.text = "Fine";
+        }
+        else
+        {
+            weatherText.text = "Snow";
+        }
+
     }
 }
